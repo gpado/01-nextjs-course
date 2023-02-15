4945517af5c0e3d5b75052ae147c4c67fada46e1
Gabriel Padoveze <53236759+gpado@users.noreply.github.com>
2023-02-14 22:32:10 -0300

    Initial commit

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Page404() {
+    return(
+        <div>
+            <h1>Você se perdeu e caiu na página 404 :0</h1>
+            <Link href="/">Home</Link>
+        </div>
+    )
+}
